3eb6b7892109ad29ecd5b96719c3b8d80e7641ab
Irvan <peang.cookie@gmail.com>
2024-09-17 10:59:13 +0700

    Day 7

---
+++ app/Models/Job.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Arr;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+  public static function list(): array
+  {
+    return [
+      [
+        'id' => 1,
+        'title' => 'Director',
+        'salary' => '$50.000'
+      ],
+      [
+        'id' => 2,
+        'title' => 'Programmer',
+        'salary' => '$10.000'
+      ],
+      [
+        'id' => 3,
+        'title' => 'Teacher',
+        'salary' => '$40.000'
+      ],
+    ];
+  }
+
+  public static function find($id): array
+  {
+    $job = Arr::first(Job::list(), fn($job) => $job['id'] == $id);
+
+    if (!$job) {
+      abort(404);
+    }
+  }
+}
