be485d307469cc6b648f2db864c76239ca3ae9ed
Irvan <peang.cookie@gmail.com>
2024-09-18 10:29:05 +0700

    Day 10

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class JobListing extends Model
+class Employer extends Model
 {
-  use HasFactory;
+    use HasFactory;
 }
