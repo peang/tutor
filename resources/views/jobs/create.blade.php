42881547a187ee42fd6bd4c7ec46ccf360f6a2a5
Irvan <peang.cookie@gmail.com>
2024-09-19 09:58:03 +0700

    Day 17

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -19,8 +19,11 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="title" id="title" autocomplete="title"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="Shift Leader">
+                                    placeholder="Shift Leader" required>
                             </div>
+                            @error('title')
+                                <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
 
@@ -31,11 +34,23 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="salary" id="salary"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="$100,000">
+                                    placeholder="$100,000" required>
                             </div>
+
+                            @error('salary')
+                                <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
                 </div>
+
+                {{-- @if ($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif --}}
             </div>
 
         </div>
--- routes/web.php
+++ routes/web.php
@@ -5,50 +5,53 @@ use App\Models\JobListing;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+  return view('home');
 });
 
 Route::get('/jobs', function () {
-    $jobs = JobListing::with('employer')->latest()->simplePaginate(5);
+  $jobs = JobListing::with('employer')->latest()->simplePaginate(5);
 
-    return view('jobs.index', [
-        'jobs' => $jobs,
-    ]);
+  return view('jobs.index', [
+    'jobs' => $jobs,
+  ]);
 });
 
 Route::get('/jobs/create', function () {
-    return view('jobs.create');
+  return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $job = JobListing::find($id);
+  $job = JobListing::find($id);
 
-    if (!$job) {
-        abort(404);
-    }
+  if (!$job) {
+    abort(404);
+  }
 
-    return view('jobs.show', [
-        'job' => $job
-    ]);
+  return view('jobs.show', [
+    'job' => $job
+  ]);
 });
 
 Route::post('/jobs', function () {
-    // dd(request()->all());
+  request()->validate([
+    'title' => ['required', 'min: 3'],
+    'salary' => ['required'],
+  ]);
 
-    JobListing::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
+  JobListing::create([
+    'title' => request('title'),
+    'salary' => request('salary'),
+    'employer_id' => 1,
+  ]);
 
-    return redirect('/jobs');
+  return redirect('/jobs');
 });
 
 
 Route::get('/about', function () {
-    return view('about');
+  return view('about');
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+  return view('contact');
 });
